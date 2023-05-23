bf2b7ab4d8f3772c061ad73e3fecde84d224f7cd
mashoun <mashoun.me@gmail.com>
2023-05-23 20:50:25 +0300

    domain in production

--- assets/components/blogs.js
+++ assets/components/blogs.js
@@ -12,17 +12,19 @@ export default {
         
         <div class="row justify-content-start g-3 my-3">
             <section class="col-12 col-md-6 col-lg-3" v-for="b in lazyBlogs" :key="b">
-                <div class="p-3 shadow-sm bg-light rounded d-flex flex-column gap-1 pop">
-                    <div class="ratio ratio-16x9"> 
-                        <img :src="b.thumbnail" :alt="b.title" class="img-fluid object-fit-cover rounded-top skeleton">
+                <a :href="b.url">
+                    <div class="p-3 shadow-sm bg-light rounded d-flex flex-column gap-1 pop">
+                        <div class="ratio ratio-16x9"> 
+                            <img :src="b.thumbnail" :alt="b.title" class="img-fluid object-fit-cover rounded-top skeleton">
+                        </div>
+                        <a class="icon-link icon-link-hover fs-6 link-underline-opacity-0 p-1 m-0" :href="b.url">
+                            <span class="text-fade">{{b.title}}</span>
+                            <i class="bi bi-arrow-right"></i>
+                        </a>
+                        <p class="text-secondary text-fade-3 fs-small px-1 m-0" style="height: 60px;">{{b.description}}</p>
+                        <time class="p-1 fs-xsmall text-secondary mono m-0">{{utilities.timo(b.date)}} </time>
                     </div>
-                    <a class="icon-link icon-link-hover fs-6 link-underline-opacity-0 p-1 m-0" :href="b.url">
-                        <span class="text-fade">{{b.title}}</span>
-                        <i class="bi bi-arrow-right"></i>
-                    </a>
-                    <p class="text-secondary text-fade-3 fs-small px-1 m-0" style="height: 60px;">{{b.description}}</p>
-                    <time class="p-1 fs-xsmall text-secondary mono m-0">{{utilities.timo(b.date)}} </time>
-                </div>
+                </a>
             </section>
             <a id="show-more" class="col-12 icon-link icon-link-hover fs-small link-underline-opacity-0 p-1 justify-content-center" :href="utilities.env('/blogs')">
                 <span class="text-fade text-center">show more</span>
--- assets/components/services.js
+++ assets/components/services.js
@@ -9,17 +9,19 @@ export default {
         </div>
         <div class="row justify-content-start g-3 my-3">
             <section class="col-12 col-lg-4" v-for="s in services" :key="s">
-                <div class="p-3 shadow-sm bg-light rounded d-flex flex-column gap-2 pop">
-                    <div class="ratio ratio-16x9"><img :src="s.thumbnail" :alt="s.title" class="object-fit-cover skeleton img-fluid rounded-top"></div>
-                    
-                    <!-- <h6 class="pop point m-0 text-primary text-fade-3 d-flex align-items-center gap-2 justify-content-between">{{s.title}} <i class="bi bi-chevron-expand"></i> </h6> -->
-                    <a class="d-flex justify-content-between align-items-center icon-link fs-6 link-underline-opacity-0 m-0" :href="s.url">
-                        <span class="text-fade">{{s.title}}</span>
-                        <i class="bi bi-chevron-expand"></i>
-                    </a>
-                    <p class="text-secondary text-fade-3 fs-small m-0" style="height: 60px;">{{s.description}}</p>
-                    
-                </div>
+                <a :href="s.url">
+                    <div class="p-3 shadow-sm bg-light rounded d-flex flex-column gap-2 pop">
+                        <div class="ratio ratio-16x9"><img :src="s.thumbnail" :alt="s.title" class="object-fit-cover skeleton img-fluid rounded-top"></div>
+                        
+                        <!-- <h6 class="pop point m-0 text-primary text-fade-3 d-flex align-items-center gap-2 justify-content-between">{{s.title}} <i class="bi bi-chevron-expand"></i> </h6> -->
+                        <a class="d-flex justify-content-between align-items-center icon-link fs-6 link-underline-opacity-0 m-0" :href="s.url">
+                            <span class="text-fade">{{s.title}}</span>
+                            <i class="bi bi-chevron-expand"></i>
+                        </a>
+                        <p class="text-secondary text-fade-3 fs-small m-0" style="height: 60px;">{{s.description}}</p>
+                        
+                    </div>
+                </a>
             </section>
         </div>
     </section> 
